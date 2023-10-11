64860d0fdb9bfe965ba84d4d0cf5f6c5ad712435
Yakunidatazu <98469959+uselessonee@users.noreply.github.com>
2023-10-11 07:23:56 +0000

    modified:   Thuc_Hanh2/Mang_1_Chieu.C

--- Thuc_Hanh2/Mang_1_Chieu.C
+++ Thuc_Hanh2/Mang_1_Chieu.C
@@ -1,48 +1,165 @@
+#include <sdtlib.h>
+
 #include <stdio.h>
-#include <conio.h>
-#include <stdlib.h>
- int* arr;
- int n;
 
-
-void Nhap_Mang()
-{
-    printf("Nhap N:");
-    scanf("%d", &n);
-
-    arr = (int*)malloc(n * sizeof(int));
-    for (int i =0; i<= sizeof(arr)/ sizeof(int); i++)
-    {
-        printf("Nhap phan tu thu %d", i);
-        scanf("%d", &arr[i]);
-    } 
-}
-
-
-void Xuat_Mang()
-{
-    for (int i =0; i<= n; i++)
-    {
-        printf("Phan tu thu i la %d \n", arr[i]);
+void nhapMang(int** arr, int n) {
+    *arr = (int*)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        printf("Nhap phan tu thu %d: ", i + 1);
+        scanf("%d", &(*arr)[i]);
     }
 }
 
-int find_Max()
-{
-    int max=0;
-    for (int i =0; i<= n; i++)
-    {
-        max = (arr[i] > max) ? arr[i] : max;
+void xuatMang(int* arr, int n) {
+    printf("Mang vua nhap la: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int timMax(int* arr, int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
     }
     return max;
 }
-int main()
-{
-    Nhap_Mang();
-    Xuat_Mang();
 
-    printf("Gia tri lon nhat la: %d \n",find_Max();
+int tinhTich(int* arr, int n) {
+    int tich = 1;
+    for (int i = 0; i < n; i++) {
+        tich *= arr[i];
+    }
+    return tich;
+}
+
+int laSoNguyenTo(int num) {
+    if (num < 2) {
+        return 0;
+    }
+    for (int i = 2; i <= num / 2; i++) {
+        if (num % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void demTongSoNguyenTo(int* arr, int n, int* count, int* sum) {
+    *count = 0;
+    *sum = 0;
+    for (int i = 0; i < n; i++) {
+        if (laSoNguyenTo(arr[i])) {
+            (*count)++;
+            (*sum) += arr[i];
+        }
+    }
+}
+
+// Hàm in vị trí các phần tử có giá trị bằng x
+void inViTri(int* arr, int n, int x) {
+    int found = 0;
+    printf("Vi tri cac phan tu co gia tri bang %d la: ", x);
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == x) {
+            printf("%d ", i);
+            found = 1;
+        }
+    }
+    if (!found) {
+        printf("Khong co phan tu nao co gia tri bang %d", x);
+    }
+    printf("\n");
+}
+
+// Hàm sắp xếp mảng tăng dần
+void sapXepTangDan(int* arr, int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] > arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+// Hàm chèn giá trị
+void chenGiaTri(int** arr, int* n, int x, int viTri) {
+    (*n)++;
+    *arr = (int*)realloc(*arr, (*n) * sizeof(int));
+    for (int i = (*n) - 1; i > viTri; i--) {
+        (*arr)[i] = (*arr)[i - 1];
+    }
+    (*arr)[viTri] = x;
+}
+
+// Hàm xóa giá trị k khỏi mảng
+void xoaGiaTri(int** arr, int* n, int k) {
+    int count = 0;
+    for (int i = 0; i < *n; i++) {
+        if ((*arr)[i] == k) {
+            count++;
+        } else {
+            (*arr)[i - count] = (*arr)[i];
+        }
+    }
+    *n -= count;
+    *arr = (int*)realloc(*arr, (*n) * sizeof(int));
+}
+
+int main() {
+    int n;
+    printf("Nhap so phan tu cua mang: ");
+    scanf("%d", &n);
+
+    int* arr;
+
+    nhapMang(&arr, n);
+    xuatMang(arr, n);
+
+    int max = timMax(arr, n);
+    printf("Gia tri lon nhat trong mang la: %d\n", max);
+
+    int tich = tinhTich(arr, n);
+    printf("Tich cac phan tu trong mang la: %d\n", tich);
+
+    int count, sum;
+    demTongSoNguyenTo(arr, n, &count, &sum);
+    printf("So phan tu nguyen to trong mang la: %d\n", count);
+    printf("Tong cac phan tu nguyen to trong mang la: %d\n", sum);
+
+    int x;
+    printf("Nhap gia tri x: ");
+    scanf("%d", &x);
+    inViTri(arr, n, x);
+
+    sapXepTangDan(arr, n);
+    printf("Mang sau khi sap xep tang dan: ");
+    xuatMang(arr, n);
+
+    int viTri, giaTri;
+    printf("Nhap gia tri can chen: ");
+    scanf("%d", &giaTri);
+    printf("Nhap vi tri can chen: ");
+    scanf("%d", &viTri);
+    chenGiaTri(&arr, &n, giaTri, viTri);
+    printf("Mang sau khi chen gia tri: ");
+    xuatMang(arr, n);
+
+    int k;
+    printf("Nhap gia tri k can xoa: ");
+    scanf("%d", &k);
+    xoaGiaTri(&arr, &n, k);
+    printf("Mang sau khi xoa gia tri: ");
+    xuatMang(arr, n);
+
     free(arr);
 
-    getch();
+    return 0;
 }
+Main Avatar

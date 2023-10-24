dec0c03e474bbefc7f324c4efd3c61a0f8c4356e
Yakunidatazu <98469959+uselessonee@users.noreply.github.com>
2023-10-25 00:18:50 +0700

    Update Thuc_Hanh_5.cpp

--- Thuc_Hanh 5-6/Thuc_Hanh_5.cpp
+++ Thuc_Hanh 5-6/Thuc_Hanh_5.cpp
@@ -1,142 +1,234 @@
-#include<stdio.h>
-#include<conio.h>
-#include<string.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
 
-//The name said it all
-void delete_char(char *str, int i) {
+char S1[50], S2[50];
+bool Run = 1, Menuprinted = 0;
+
+void Input_string()
+{
+    printf("Nhap chuoi S1:");
+    gets(S1);
+
+    printf("Nhap chuoi S2:");
+    gets(S2);
+}
+// The name said it all
+void delete_char(char *str, int i)
+{
     int len = strlen(str);
 
-    for (; i < len -1; i++)
+    for (; i < len - 1; i++)
     {
-       str[i] = str[i+1];
+        str[i] = str[i + 1];
     }
-    
-    
+
     str[i] = '\0';
 }
 
 void String_Norm(char *Str)
 {
-	
-	while (Str[0] == ' ')
-	{
-		delete_char(Str, 0);
-	}
-	
-	Str[0] = toupper(Str[0]);
-	
-	for(int i =0; i< strlen(Str) -1; i++)
-	{
-		while (Str[i]==' ' && Str[i+1]==' ')
-		{
-			delete_char(Str, i);
-		}
-		if (Str[i]==' ' && Str[i+1]!=' ')
-		{
-			Str[i+1]=toupper(Str[i+1]);
-		}
-	}
-	
-	if (Str[strlen(Str) -1] == ' ')
-	{
-		delete_char(Str, 0);
-	}
-	
-	printf("%s", Str);
+
+    while (Str[0] == ' ')
+    {
+        delete_char(Str, 0);
+    }
+
+    Str[0] = toupper(Str[0]);
+
+    for (int i = 0; i < strlen(Str) - 1; i++)
+    {
+        while (Str[i] == ' ' && Str[i + 1] == ' ')
+        {
+            delete_char(Str, i);
+        }
+        if (Str[i] == ' ' && Str[i + 1] != ' ')
+        {
+            Str[i + 1] = toupper(Str[i + 1]);
+        }
+    }
+
+    if (Str[strlen(Str) - 1] == ' ')
+    {
+        delete_char(Str, 0);
+    }
+
+    printf("%s", Str);
 }
 
-
-int Count_S2_in_S1(char* S1, char* S2)
+int Count_S2_in_S1(char *S1, char *S2)
 {
-	int count =0;
+    int count = 0;
 
-	for (int i =0; S1[i] != '\0'; i++)
-	{
-		if (S1[i] == S2[0])
-		{
-			for (int j =0; (S2[j] != '\0') ; j++)
-			{
-				if(S1[i+j] != S2[j])
-				{
-					break;
-				}
-				if(S2[j+1] == '\0')
-				{
-					count++;
-				}
-			}
-		}
-	}
-	return count;
+    for (int i = 0; S1[i] != '\0'; i++)
+    {
+        if (S1[i] == S2[0])
+        {
+            for (int j = 0; (S2[j] != '\0'); j++)
+            {
+                if (S1[i + j] != S2[j])
+                {
+                    break;
+                }
+                if (S2[j + 1] == '\0')
+                {
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
 }
 
-
-int Count_Char(char* Str)
+int Count_Char(char *Str)
 {
-	char x;
-	int count =0;
+    char x;
+    int count = 0;
 
-	printf("Nhap ky tu can tim:");
-	scanf("%c", &x);
+    printf("Nhap ky tu can tim:");
+    scanf("%c", &x);
 
-	x = tolower(x);
+    x = tolower(x);
 
-	int leng = strlen(Str);
+    int leng = strlen(Str);
 
-	for(int i =0; i< leng; i++)
-	{
-		if(tolower(Str[i]) == x)
-		{
-			count++;
-		}
-	}
-	return count;
+    for (int i = 0; i < leng; i++)
+    {
+        if (tolower(Str[i]) == x)
+        {
+            count++;
+        }
+    }
+    return count;
 }
 
-int count_word(char* Str)
+int count_word(char *Str)
 {
-	int count;
+    int count;
 
-	for ( int i =0; Str[i] != '\0'; i++)
-	{
-		if (Str[i] == ' ') count++;
-	}
+    for (int i = 0; Str[i] != '\0'; i++)
+    {
+        if (Str[i] == ' ')
+            count++;
+    }
 
-	return count++;
+    return count++;
 }
 
-
-
-void link_char(char* S1,char* S2)
+void link_char(char *S1, char *S2)
 {
-	int leng = strlen(S1);
-	for (int i = leng; S2[i-leng] != '\0'; i++)
-	{
-		S1[i] = S2[i-leng];
-	}
+    int leng = strlen(S1);
+    for (int i = leng; S2[i - leng] != '\0'; i++)
+    {
+        S1[i] = S2[i - leng];
+    }
 
-	printf("%s", S1);
+    printf("%s", S1);
+}
+
+bool is_symmetrical(char *str)
+{
+    int length = strlen(str);
+    int mid = length / 2;
+
+    for (int i = 0; i < mid; i++)
+    {
+        if (str[i] != str[length - i - 1])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void Reverse_String(char *str)
+{
+    int length = strlen(str);
+
+    for (int i = length; i >= 0; i--)
+    {
+        printf("%c", str[i]);
+    }
+}
+
+// Menu
+
+void Menu()
+{
+    system("cls");
+    printf("************************************\n");
+    printf("** THAO TAC TREN CHUOI            **\n");
+    printf("** 1. Nhap va chuan hoa chuoi     **\n");
+    printf("** 2. Dem so tu trong chuoi       **\n");
+    printf("** 3. Ghep chuoi                  **\n");
+    printf("** 4. Kiem tra doi xung           **\n");
+    printf("** 5. In chuoi dao nguoc          **\n");
+    printf("** 0. Thoat                       **\n");
+    printf("*************************************\n");
+}
+
+void Input()
+{
+    // Input
+    if (_kbhit())
+    {
+        switch (getch())
+        {
+        case '1':
+            Input_string();
+            printf("Chuoi S1 sau khi chuan hoa:\n");
+            String_Norm(S1);
+            printf("\nChuoi S1 sau khi chuan hoa:\n");
+            String_Norm(S2);
+
+            printf("\nEnter de tiep tuc\n");
+            getch();
+            break;
+        case '2':
+            printf("Dem so tu: %d", count_word(S1));
+            printf("\nEnter de tiep tuc\n");
+            getch();
+            break;
+        case '3':
+            link_char(S1, S2);
+            printf("\nEnter de tiep tuc\n");
+            getch();
+            break;
+        case '4':
+            if (is_symmetrical(S1))
+                printf("S1 doi xung");
+            else
+                printf("S1 ko doi xung");
+            printf("\nEnter de tiep tuc\n");
+            getch();
+            break;
+        case '5':
+            Reverse_String(S1);
+            printf("\nEnter de tiep tuc\n");
+            getch();
+            break;
+        case '0':
+            Run = 0;
+            break;
+        default:
+            break;
+        }
+        Menuprinted = false;
+    }
 }
 
 int main()
 {
-	char S1[50], S2[50];
-	printf("Nhap chuoi S1:");
-	gets(S1);
-	
-	printf("Nhap chuoi S2:");
-	gets(S2);
-	
-	printf("Chuoi sau khi chuan hoa:");
-	printf("\nS1:");String_Norm(S1);
-	
-	printf("\nS2:");String_Norm(S2);
-	
-	printf("\n %d", Count_S2_in_S1(S1,S2));
-	
-	
-	printf("%d ", Count_Char(S1));
-
-	printf("Dem tu:%d", count_word(S1));
+    while (Run)
+    {
+        if (!Menuprinted)
+        {
+            Menu();
+            Menuprinted = true;
+        }
+        Input();
+    }
 }
